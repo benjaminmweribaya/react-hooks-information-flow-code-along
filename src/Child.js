68837fb7649197d73d903cf56e1782d495c06399
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-11-02 00:02:57 +0300

    feat: Understand the flow of information between components with props Use callback functions as props to update state in a parent component

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  // Generates a new color and triggers the color change in the parent
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);  // Passes the new color to the parent
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
+
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,20 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
-  const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(getRandomColor());
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  // Update both the parent and child colors when a child is clicked
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);         // Change Parent's color
+    setChildrenColor(newChildColor);   // Update both children's color
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
